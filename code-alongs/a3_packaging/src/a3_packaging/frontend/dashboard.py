d90d2da1ac851cbe949badcfbbac08784f240d6b
tasmina@hotmail.com <tasmina@hotmail.com>
2026-01-09 08:17:05 +0100

    08

---
+++ code-alongs/a3_packaging/src/a3_packaging/frontend/dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import httpx
+
+data = httpx.get("http://127.0.0.1:8000/taxi/")
+
+
+df = pd.DataFrame(data.json())
+
+
+def main():
+    st.markdown("# Taxi Prediction Dashboard")
+
+    st.dataframe(df)
+
+
+if __name__ == "__main__":
+    main()
---
+++ code-alongs/a3_packaging/src/a3_packaging/utils/helpers.py
@@ -0,0 +1,2 @@
+
+# helper and utility functions goes here
